aac43acc15ff7bc69a272faac072daa77884f863
YuriBorrmann <40867645+YuriBorrmann@users.noreply.github.com>
2025-09-21 13:59:14 -0300

    Add files via upload

--- backend/services/logger/logger.py
+++ backend/services/logger/logger.py
@@ -21,3 +21,15 @@ class SimpleLogger:
             self.logger.error(message)
         elif level == "DEBUG":
             self.logger.debug(message)
+            
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
